6b307b599396ef4e89d1a1db7d19d653bf7068f1
umepab <umepab@yandex.ru>
2017-09-26 17:36:54 +0300

    sendmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,8 @@ Rails.application.configure do
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
+  config.action_mailer.default_url_options = { host: 'actualtask.com' }
+  config.action_mailer.delivery_method = :send_mail
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
