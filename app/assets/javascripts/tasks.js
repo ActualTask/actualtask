7e943a24f4ee3115a9fa784b0f6b9d39ff6c4fa4
umepab <umepab@yandex.ru>
2017-10-12 19:23:01 +0300

    autocomlete ok

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,11 +1,18 @@
-$(document).ready(function(){
+var ready;
+ready = function () {
     $("[id $= 'address']").each(function(){
         var input = document.getElementById($(this).attr('id'));
+        console.log(input);
         autocomplete = new google.maps.places.Autocomplete(input);
     });
     $(document).on("fields_added.nested_form_fields", function(event, param) {
+        console.log('input');
         var id = "task_locations_attributes_"+param.added_index+"_address";
         var input = document.getElementById(id);
+        console.log(input);
         autocomplete = new google.maps.places.Autocomplete(input);
     });
-});
+};
+
+jQuery(document).ready(ready);
+$(document).on('turbolinks:load', ready);
